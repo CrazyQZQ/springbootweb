8289905f488edae24e0c0b76972794b67d2e88fb
qq <13186061107@163.com>
2019-10-05 18:20:49 +0800

    添加多文件上传

--- src/main/java/com/example/springbootweb/Controller/FileUploadController.java
+++ src/main/java/com/example/springbootweb/Controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.example.springbootweb.Controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,9 +13,17 @@ import java.util.Date;
 import java.util.UUID;
 
 @RestController
+@Slf4j
 public class FileUploadController {
 
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+    /**
+     * 文件上传
+     * @param file
+     * @param req
+     * @return
+     */
     @PostMapping("/upload")
     public String upload(MultipartFile file, HttpServletRequest req){
         String format = sdf.format(new Date());
@@ -27,10 +36,39 @@ public class FileUploadController {
         String newName = UUID.randomUUID().toString() + oldName.substring(oldName.indexOf("."));
         try {
             file.transferTo(new File(folder,newName));
-            return req.getScheme() + "://" +req.getServerName() + ":" +req.getServerPort() + "/img/"+ format + newName;
+            return req.getScheme() + "://" +req.getServerName() + ":" +req.getServerPort() + "/img"+ format + "/" + newName;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "error";
     }
+
+    /**
+     * 多文件上传 （文件名相同时用MultipartFile[]，不同时写多个MultipartFile）
+     * @param files
+     * @param req
+     * @return
+     */
+    @PostMapping("/uploads")
+    public String uploads(MultipartFile[] files, HttpServletRequest req){
+        String format = sdf.format(new Date());
+        String realPath = req.getServletContext().getRealPath("/img")+format;
+        File folder = new File(realPath);
+        if(!folder.exists()){
+            folder.mkdirs();
+        }
+        for (MultipartFile file : files) {
+            String oldName = file.getOriginalFilename();
+            String newName = UUID.randomUUID().toString() + oldName.substring(oldName.indexOf("."));
+            try {
+                file.transferTo(new File(folder,newName));
+                String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/img" + format + "/" + newName;
+                log.info(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "success";
+    }
+
 }
