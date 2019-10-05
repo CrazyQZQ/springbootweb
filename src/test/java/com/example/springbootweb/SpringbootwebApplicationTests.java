5f8cce87971402243159170b034826b62261c281
qq <13186061107@163.com>
2019-10-05 17:32:05 +0800

    使用lombok插件

--- src/test/java/com/example/springbootweb/SpringbootwebApplicationTests.java
+++ src/test/java/com/example/springbootweb/SpringbootwebApplicationTests.java
@@ -1,16 +1,28 @@
 package com.example.springbootweb;
 
+import com.example.springbootweb.bean.User;
+import com.example.springbootweb.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SpringbootwebApplicationTests {
-
+    @Autowired
+    UserService us;
     @Test
     public void contextLoads() {
+        List<User> allUsers = us.getAllUsers();
+        allUsers.forEach(user -> {
+            log.info(user.toString());
+        });
     }
 
 }
